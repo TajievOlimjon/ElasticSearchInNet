fc2c9f413d1c88da6cfa4c21ad218a9c895b341b
TajievOlimjon <tajiev0711@gmail.com>
2023-10-20 11:34:03 +0500

    aded entity,DTOs,filter ,services and controller

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,15 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(configure =>
+{
+    configure.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IStudentService,StudentService>();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +22,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
